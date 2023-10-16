8929829263a998fd55130e131f663ff21ba5c10a
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-10-16 07:16:16 -0700

    now includes Elise's video and class work from week 2

---
+++ week2_script.R
@@ -0,0 +1,71 @@
+#tour of console, environment, git tab, files/plots/help
+#console vs script
+#week 2 intro to R functionality 
+
+1 + 100
+1 - 
+3
+
+7^2 + 3
+exp(3)
+ 
+2/100000
+4e3
+
+mynum <- 50
+
+mynum
+
+myword <- "hello"
+mynum <- 80
+
+mynum
+
+mynum = 92
+
+mynum
+
+cos(0)
+#is this radians or degrees? let's find out
+?cos
+
+log(5)
+log(100, base = 10)
+log(10,100)
+log(x = 6, base = 10)
+log(base = 10, x = 6)
+
+log_of_negative <- log(-2)
+#gives a warning
+
+exp(0.5)
+
+30 <=30
+30 < 30
+45 >= 45
+45 > 45
+log_of_negative > 3
+3 < "hello"
+30 != 7
+40 == 40
+
+class(40)
+class(40.0)
+class(40L)
+40==40.0
+as.integer(40)==40.0
+
+identical(as.integer(40), 40.00)
+identical(40L, 40.0)
+
+weightOfMyElephant
+weight_of_my_elephant
+
+elephant1_kg <- 3492
+elephant2_lb <- 7757
+
+#this is a note to myself
+#let's use tab completion:
+lb_per_kg <- 2.2
+
+
