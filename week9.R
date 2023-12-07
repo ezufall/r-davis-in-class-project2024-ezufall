ae77ae89f787edfb38c0c03ac2f92016c4e5222c
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2023-12-07 12:31:18 -0800

    added pseudocode instructions

--- week9.R
+++ week9.R
@@ -6,6 +6,14 @@ surveys <- readr::read_csv("data/portal_data_joined.csv")
 mloa <- readr::read_csv("https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
 
 #part 1
+#Using a for loop, print to the console the longest species name of each taxon. 
+#Hint: the function nchar() gets you the number of characters in a string.
+
+#task: find the species name with the longest number of characters in surveys$speices
+#aka the species name where the length of its species name is == to the max length of any species name
+#   surveys %>% filter(nchar(surveys$species) == max(nchar(surveys$species))) %>% select(species)
+#but that's the overall longest. we want the longest within each taxon, so first we have to subset surveys
+#by each taxa
 
 #how to make a forloop? First make one case:
 i <- unique(surveys$taxa)[1]
@@ -20,9 +28,9 @@ mytaxon <- surveys[surveys$taxa == i,]
 #look at the boolean vector used to subset the dataframe
 nchar(mytaxon$species) == max(nchar(mytaxon$species))
 
-longestnames <- mytaxon[nchar(mytaxon$species) == max(nchar(mytaxon$species)),] %>% select(species)
+longestnames <- mytaxon %>% filter(nchar(mytaxon$species) == max(nchar(mytaxon$species))) %>% select(species)
 #or
-#mytaxon %>% filter(nchar(mytaxon$species) == max(nchar(mytaxon$species))) %>% select(species)
+#mytaxon[nchar(mytaxon$species) == max(nchar(mytaxon$species)),] %>% select(species)
 #or
 #mytaxon$species[nchar(mytaxon$species) == max(nchar(mytaxon$species))] 
 
@@ -35,6 +43,11 @@ for(i in unique(surveys$taxa)){
   print(paste0("The longest species name(s) among ", i, "s is/are: "))
   print(unique(longestnames$species))
 }
+
+#spoiler alert: this is how you do it without a for loop in the tidyverse
+surveys %>% group_by(taxa) %>% mutate(longestintaxa = max(nchar(species))) %>% 
+  filter(nchar(species) == longestintaxa) %>% select(species) %>% unique()
+
 
 #part 2
 mycols <- mloa %>% select("windDir","windSpeed_m_s","baro_hPa","temp_C_2m","temp_C_10m","temp_C_towertop","rel_humid", "precip_intens_mm_hr")
